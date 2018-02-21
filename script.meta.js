dfb7bd2b92558710f370e348fd92f9b0a05a4bb1
Fabio P <fp555@users.noreply.github.com>
2018-02-21 19:38:46 +0100

    Initial commit

---
+++ script.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name			  E(x)Hentai Tags Preview
+// @author      fp555
+// @namespace		exhtp
+// @version			1.0
+// @description	Adds a preview of gallery tags on hover.
+// @include			https://exhentai.org/
+// @include			https://exhentai.org/?*
+// @include			https://exhentai.org/tag/*
+// @include			https://exhentai.org/favorites.php
+// @include			https://exhentai.org/favorites.php?*
+// @include			https://exhentai.org/uploader/*
+// @include			https://e-hentai.org/
+// @include			https://e-hentai.org/?*
+// @include			https://e-hentai.org/tag/*
+// @include			https://e-hentai.org/favorites.php
+// @include			https://e-hentai.org/favorites.php?*
+// @include			https://e-hentai.org/uploader/*
+// @grant       none
+// @updateURL   https://raw.githubusercontent.com/fp555/exh-tags-preview/master/script.meta.js
+// @downloadURL https://raw.githubusercontent.com/fp555/exh-tags-preview/master/script.user.js
+// @homepage    https://github.com/fp555/exh-tags-preview
+// ==/UserScript==
