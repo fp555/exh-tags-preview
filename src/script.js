154fcddc15d3af887de2f23cbe928b99bf80ec22
* <7722100+fp555@users.noreply.github.com>
2020-06-21 18:11:18 +0200

    implemented icon mode

--- src/script.js
+++ src/script.js
@@ -1,21 +1,13 @@
-(function() {
+((mode, views) => {
     "use strict";
     
-    const $ = {
-        legacy: {_s: ".glink", _l(e) {return e.target.parentElement.href}},
-        icon: {_s: ".glname > span:last-child", _l(e) {return e.target.parentElement.href}}
-    };
-    const mode = GM_getValue("exhtp.mode", (() => {GM_setValue("exhtp.mode", "legacy"); "legacy";})());
-    const views = GM_getValue("exhtp.views", (() => {GM_setValue("exhtp.views", ["m", "p", "t"]); ["m", "p", "t"];})());
     const tt = document.body.appendChild(document.createElement("div"));
     const xhr = new XMLHttpRequest();
     const mef = (e) => {
-        xhr.open("GET", $[mode]._l(e));
+        xhr.open("GET", e.target.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href);
         xhr.responseType = "document";
         xhr.onload = () => {
             tt.innerHTML = xhr.responseXML.querySelector("div#taglist").innerHTML;
-            //console.log("pageY:" + e.pageY + " screenheight:" + window.innerHeight + " clientY:" + e.clientY + " tooltipY:" + tt.offsetHeight);
-            //console.log("pageX:" + e.pageX + " screenwidth:" + window.innerWidth + " screenX:" + e.clientX + " tooltipX:" + tt.offsetWidth);
             tt.style.top = `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`;
             tt.style.left = `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`;
             tt.style.visibility = "visible";
@@ -28,23 +20,21 @@
     };
     
     /*main*/
-    xhr.open("GET", GM_getResourceURL("exhcss"));
-    xhr.responseType = "text";
-    xhr.onload = () => {
-        GM_addStyle(xhr.responseText);
-        tt.id = "info_div";
-        tt.classList.add((window.location.toString().indexOf("exhentai.org") >= 0)? "ex" : "eh");
-        if(views.includes(document.querySelector("#dms option[selected]").value)) for(let g of document.querySelectorAll(".glink")) {
-            g.addEventListener("mouseenter", mef);
-            g.addEventListener("mouseleave", mlf);
-        }
-    };
-    xhr.send();
-    //tests
-    for(let g of document.querySelectorAll(".glname")) {
-        g.style.display = "flex";
-        let icon = g.appendChild(document.createElement("span"));
-        icon.innerHTML = "&#x1F441;&#xFE0F;&#x200D;&#x1F5E8;&#xFE0F;";
-        icon.style.cssText = "font-size: 14pt;margin-left:auto;padding-left: 8px;flex: 0 0 23px;cursor: pointer;align-self: center;";
+    GM_getValue("exhtp.mode", false) || GM_setValue("exhtp.mode", mode);
+    GM_getValue("exhtp.views", false) || GM_setValue("exhtp.views", views);
+    if(views.includes(document.querySelector("#dms option[selected]").value) && ["legacy", "icon"].includes(mode)) {
+        xhr.open("GET", GM_getResourceURL("exhcss"));
+        xhr.responseType = "text";
+        xhr.onload = () => {
+            GM_addStyle(xhr.responseText);
+            tt.id = "info_div";
+            tt.classList.add((window.location.toString().indexOf("exhentai.org") >= 0)? "ex" : "eh");
+            if(mode === "icon") for(let g of document.querySelectorAll(".glname")) g.appendChild(document.createElement("span")).innerHTML = "&#x1F441;&#xFE0F;&#x200D;&#x1F5E8;&#xFE0F;";
+            for(let g of document.querySelectorAll((mode === "icon")? ".glname > span:last-child" : ".glink")) {
+                g.addEventListener("mouseenter", mef);
+                g.addEventListener("mouseleave", mlf);
+            }
+        };
+        xhr.send();
     }
-})();
+})(GM_getValue("exhtp.mode", "legacy"), GM_getValue("exhtp.views", ["m", "p", "l", "t"]));
