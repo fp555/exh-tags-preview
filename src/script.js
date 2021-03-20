906e40b01369599e443f7124c655a8f8fc42a0d4
junker88b <7722100+fp555@users.noreply.github.com>
2021-03-20 15:24:08 +0100

    Changes for v1.7.2
    * preparing new configuration interface
    * fix settings validation bug

--- src/script.js
+++ src/script.js
@@ -1,44 +1,25 @@
-(config => {
+((config) => {
     "use strict";
     
-    const tt = document.body.appendChild(document.createElement("div"));
-    const css = GM_getResourceText("exhcss");
-    const fjords = window.location.toString().indexOf("exhentai.org") >= 0;
+    const css = GM_getResourceText("style");
+    const content = document.createRange().createContextualFragment(GM_getResourceText("content"));
     const curview = document.querySelector("#dms option[selected]").value;
-    const options = {
-        mode: ["legacy", "icon"],
-        views: "mplt", // "e"xtended is not supported
-        newtab: [true, false]
-    };
-    const mef = (e) => {
-        GM_xmlhttpRequest({
-            url: e.target.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href,
-            method: "GET",
-            responseType: "document",
-            anonymous: false,
-            onload: (xhr) => {
-                tt.innerHTML = xhr.response.querySelector("div#taglist").innerHTML;
-                tt.style.top = `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`;
-                tt.style.left = `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`;
-                tt.style.visibility = "visible";
-            }
-        });
-    };
-    const mlf = () => {
-        tt.style.visibility = "hidden";
-        tt.textContent = "Loading...";
-    };
     
-    /*main*/
-    for(const c in config) {
+    for(const c in config) { // check for valid storage values
+        const options = {
+            mode: ["legacy","icon"],
+            views: ["m","p","l","t"], // "e"xtended is not supported
+            newtab: [true,false]
+        };
         GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, config[c]);
-        if(!options[c].includes(config[c])) {
-            console.error("Unrecognized config " + config[c] + " for " + c + ". Clearing from storage");
+        const cval = Array.isArray(config[c])? config[c] : [config[c]];
+        if(!cval.every((x) => (options[c]).includes(x))) {
+            console.error("Unrecognized option " + x + " for " + c + ". Clearing from storage");
             GM_deleteValue("exhtp." + c);
             return;
         }
     }
-    if(config.newtab) {
+    if(config.newtab) { // open galleries in a new tab
         const sel = ({
             m: ".gl3m.glname>a",
             p: ".gl3m.glname>a",
@@ -46,16 +27,40 @@
             e: ".gl1e>div>a,.gl2e>div>a",
             t: ".gl1t>a,.gl4t.glname>div>a,.gl3t>a"
         })[curview];
-        for(const gl of document.body.querySelectorAll(sel)) gl.setAttribute("target", "_blank");
+        document.body.querySelectorAll(sel).forEach((gl) => gl.setAttribute("target", "_blank"));
     }
+    
+    /* main */
     if(config.views.includes(curview)) {
         GM_addStyle(css);
-        tt.id = "info_div";
-        tt.classList.add(fjords? "ex" : "eh");
-        if(config.mode === "icon") for(let g of document.querySelectorAll(".glname")) g.appendChild(document.createElement("span")).innerHTML = "&#x1F441;&#xFE0F;&#x200D;&#x1F5E8;&#xFE0F;";
-        for(let g of document.querySelectorAll((config.mode === "icon")? ".glname > span:last-child" : ".glink")) {
-            g.addEventListener("mouseenter", mef);
-            g.addEventListener("mouseleave", mlf);
-        }
+        document.body.appendChild(content);
+        const tt = document.querySelector("#tagstt");
+        const ttopt = document.forms.ttopt;
+        [tt, ttopt].forEach((el) => el.classList.add(window.location.href.indexOf("exhentai.org") >= 0 ? "exstyle" : "ehstyle"));
+        if(config.mode === "icon") document.querySelectorAll(".glname").forEach((i) => i.appendChild(document.createElement("span")).innerHTML = "&#x1F441;&#xFE0F;&#x200D;&#x1F5E8;&#xFE0F;");
+        document.querySelectorAll(config.mode === "icon"? ".glname>span:last-child" : ".glink").forEach((g) => {
+            g.addEventListener("mouseenter", (e) => {
+                GM_xmlhttpRequest({
+                    url: e.currentTarget.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href,
+                    method: "GET",
+                    responseType: "document",
+                    anonymous: false,
+                    onload: (xhr) => {
+                        tt.innerHTML = xhr.response.querySelector("div#taglist").innerHTML;
+                        tt.style.top = `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`;
+                        tt.style.left = `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`;
+                        tt.style.visibility = "visible";
+                    }
+                });
+            });
+            g.addEventListener("mouseleave", () => {
+                tt.style.visibility = "hidden";
+                tt.textContent = "Loading...";
+            });
+        });
     }
-})({mode: GM_getValue("exhtp.mode", "legacy"), views: GM_getValue("exhtp.views", "mplt"), newtab: GM_getValue("exhtp.newtab", true)});
+})(Object.freeze({
+    mode: GM_getValue("exhtp.mode", "legacy"),
+    views: GM_getValue("exhtp.views", ["m"]),
+    newtab: GM_getValue("exhtp.newtab", true)
+}));
