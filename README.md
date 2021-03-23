a1497458cfe60af7a22beb0b168ba2f44ac68c42
junker88b <7722100+fp555@users.noreply.github.com>
2021-03-23 23:53:30 +0100

    - include license in userscript build - use bubbling pointer events - change page with documentfragment - config panel - manage tooltip with css vars - awaiting promises and async functions

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
 # E(x)-Hentai Tags Preview :panda_face::label::mag:
-This is an unofficial userscript for e-hentai/exhentai (**NSFW sites** so no links) that displays related tags when you hover over a gallery link. If you can't be bothered opening galleries every time to see all tags and you don't want to use the native *Extended* layout, this is the script for you.
+This is an unofficial userscript for e-hentai/exhentai (**NSFW sites** so no links) that displays related tags when you hover over a gallery link. If you can't be bothered opening galleries every time to see all tags and you don't want to use the native *Extended* layout, this is the script for you. This strives to be the smallest and most straightforward script that could possibly do a decent enough job.
 
 ## Install
-If your browser does not natively support userscripts, install something like [Tampermonkey](https://tampermonkey.net/) or [Violentmonkey](https://violentmonkey.github.io/get-it/), then go grab the [latest userscript release](https://github.com/fp555/exh-tags-preview/releases/latest).
+If your browser does not natively support userscripts, install something like [Tampermonkey](https://tampermonkey.net/) or [Violentmonkey](https://violentmonkey.github.io/get-it/), then go grab the [latest userscript release](https://github.com/fp555/exh-tags-preview/releases/latest/download/script.user.js).
 
 ## Configuration
-Starting with v1.6.0 the script behavior can be customized by editing the stored values from your userscripts manager:
-- **Tampermonkey**: open the Dashboard and click at the script's name, then select the *Storage* tab;
-- **Violentmonkey**: open the Dashboard and click the Edit button under the script's name, then select the *Values* tab;
-- *anything else*: RTFM.
-
-Run the script once to let it setup its storage. [Check your release notes](https://github.com/fp555/exh-tags-preview/releases) for details on what can be customized.
+The script will autoconfigure the default settings on first run. Starting with v1.8.0 there is an options button at the bottom right corner of the webpage. [Check your release notes](https://github.com/fp555/exh-tags-preview/releases) for more details on available settings.
 
 ## Troubleshooting
 ### I found a bug! / It doesn't work!
-- Stop using weird browsers that don't respect/implement basic web standards;
-  - While I hear it may be possible to run userscripts in Internet Explorer, I expect mine not to work properly. I don't care about IE compatibility, and neither should you!
-- Try using Tampermonkey or Violentmonkey as your userscript manager. Anything else is unsupported and you're on your own;
-- This script uses emojis: [make sure](http://caniemoji.com/) your browser/OS combination can display them properly;
-- On E-Hentai the popup won't show up for some galleries. This is because of the "offensive content" warning that is related to certain specific tags. To fix this you have to click "**Never warn me again**" on the warning page. Of course, as the page says, if you do so you lose ALL rights to be offended/traumatized. Alternatively you can use Exhentai like all the other cool kids on the Internet :sunglasses:;
-- [Open an issue](https://github.com/fp555/exh-tags-preview/issues/new) and convince me it's my fault.
+Before [opening an issue](https://github.com/fp555/exh-tags-preview/issues/new) please:
+1) Stop using weird and outdated browsers that don't support basic W3C standards or modern CSS/JS features;
+2) Make sure your userscript manager is updated to the latest available version;
+3) Make sure you are using the latest release of the userscript;
+4) This script uses emojis: [make sure](http://caniemoji.com/) your browser/OS combination can display them properly.
 
-### Hey I'd like you to add...
-[Open an issue](https://github.com/fp555/exh-tags-preview/issues/new) and convince me to do it. Even better: [create a pull request](https://help.github.com/articles/creating-a-pull-request/).
+### Known issues/limitations
+- The popup will be empty if a gallery has no tags at all. Please be a useful member of the community and add some yourself;
+- Empty popups for galleries that trigger the "offensive content" warning if you have it enabled. Make sure you **clearly** understand the risks before disabling it;
+- Empty popups for inaccessible (permanently deleted, copyright claimed, banned in your country...) galleries.
 
-### Privacy policy (is this spyware?)
-I couldn't care less about your disgusting fetishes: I already have mine, and I can guarantee they're objectively better than yours :smirk:. Your questionable browsing history shall remain between you and the NSA. Just [check the code yourself](https://github.com/fp555/exh-tags-preview/blob/master/src/script.js): this userscript is so trivial I didn't even bother including jQuery, and since it was originally made for personal use and convenience the possibility of harvesting user data never even crossed my mind.
+### Hey why this doesn't have `$feature`?
+[Open an issue](https://github.com/fp555/exh-tags-preview/issues/new) and convince me to work on it. Even better: [create a pull request](https://help.github.com/articles/creating-a-pull-request/).
+
+### Is this spyware? (Privacy policy)
+I couldn't care less about your disgusting fetishes: I already have mine, and I can guarantee they're objectively better than yours :smirk:. Your questionable browsing history shall remain between you and your assigned glow-in-the-dark agent from your domestic brand of alphabet agencies. This userscript was originally made for personal use and convenience, and the idea of collecting user data has never crossed my mind. This userscript has nothing up his sleeve, mainly because it's so small and simple it doesn't even *have* sleeves.
 
 ### How do I get past the panda?
 LURK MOAR NEWFAG
+
+## License
+[MIT](https://github.com/fp555/exh-tags-preview/raw/master/LICENSE)
--- src/script.js
+++ src/script.js
@@ -4,62 +4,68 @@
     const css = GM_getResourceText("style");
     const content = document.createRange().createContextualFragment(GM_getResourceText("content"));
     const curview = document.querySelector("#dms option[selected]").value;
+    const options = {
+        mode: ["legacy","icon"],
+        views: ["m","p","l","t"], // "e"xtended is not supported
+        newtab: [true,false]
+    };
     
     for(const c in config) { // check for valid storage values
-        const options = {
-            mode: ["legacy","icon"],
-            views: ["m","p","l","t"], // "e"xtended is not supported
-            newtab: [true,false]
-        };
-        GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, config[c]);
-        const cval = Array.isArray(config[c])? config[c] : [config[c]];
-        if(!cval.every((x) => (options[c]).includes(x))) {
-            console.error("Unrecognized option " + x + " for " + c + ". Clearing from storage");
-            GM_deleteValue("exhtp." + c);
-            return;
-        }
+        GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, config[c]); // set defaults on 1st run
+        (Array.isArray(config[c])? config[c] : [config[c]]).forEach((x) => {
+            if(options[c].includes(x)) content.querySelector(`input[name=${c}][value=${x}]`).checked = true; // init options panel
+            else {
+                console.error("Unrecognized option " + x + " for " + c + ". Clearing from storage");
+                GM_deleteValue("exhtp." + c);
+                return;
+            }
+        });
     }
-    if(config.newtab) { // open galleries in a new tab
-        const sel = ({
-            m: ".gl3m.glname>a",
-            p: ".gl3m.glname>a",
-            l: ".gl3c.glname>a",
-            e: ".gl1e>div>a,.gl2e>div>a",
-            t: ".gl1t>a,.gl4t.glname>div>a,.gl3t>a"
-        })[curview];
-        document.body.querySelectorAll(sel).forEach((gl) => gl.setAttribute("target", "_blank"));
-    }
+    content.querySelector("form").onchange = (e) => { // option panel wiring
+        e.stopPropagation(); // change events will all bubble here, no need to add listeners to all checkboxes
+        GM_setValue("exhtp.mode", document.querySelector("input[name=mode]:checked").value);
+        GM_setValue("exhtp.views", [...document.querySelectorAll("input[name=views]:checked")].map((v) => v.value));
+        GM_setValue("exhtp.newtab", document.querySelector("input[name=newtab]").checked);
+    };
     
     /* main */
     if(config.views.includes(curview)) {
         GM_addStyle(css);
-        document.body.appendChild(content);
-        const tt = document.querySelector("#tagstt");
-        const ttopt = document.forms.ttopt;
-        [tt, ttopt].forEach((el) => el.classList.add(window.location.href.indexOf("exhentai.org") >= 0 ? "exstyle" : "ehstyle"));
-        if(config.mode === "icon") document.querySelectorAll(".glname").forEach((i) => i.appendChild(document.createElement("span")).innerHTML = "&#x1F441;&#xFE0F;&#x200D;&#x1F5E8;&#xFE0F;");
-        document.querySelectorAll(config.mode === "icon"? ".glname>span:last-child" : ".glink").forEach((g) => {
-            g.addEventListener("mouseenter", (e) => {
-                GM_xmlhttpRequest({
-                    url: e.currentTarget.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href,
-                    method: "GET",
-                    responseType: "document",
-                    anonymous: false,
-                    onload: (xhr) => {
-                        tt.innerHTML = xhr.response.querySelector("div#taglist").innerHTML;
-                        tt.style.top = `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`;
-                        tt.style.left = `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`;
-                        tt.style.visibility = "visible";
-                    }
-                });
-            });
-            g.addEventListener("mouseleave", () => {
-                tt.style.visibility = "hidden";
-                tt.textContent = "Loading...";
-            });
+        const tt = content.querySelector("#tagstt");
+        const ttopt = content.querySelector("#ttopt");
+        [tt, ttopt].forEach((el) => el.classList.add(window.location.href.indexOf("exhentai.org") >= 0? "exstyle" : "ehstyle"));
+        document.body.appendChild(content); // since it is now empty we can reuse this documentfragment
+        content.replaceChildren(...document.querySelectorAll(".itg>*")); // temp moving page elements outside DOM for performance
+        if(config.newtab) {
+            const sel = ({
+                m: ".gl3m.glname>a",
+                p: ".gl3m.glname>a",
+                l: ".gl3c.glname>a",
+                e: ".gl1e>div>a,.gl2e>div>a",
+                t: ".gl1t>a,.gl4t.glname>div>a,.gl3t>a"
+            })[curview];
+            content.querySelectorAll(sel).forEach((gl) => gl.setAttribute("target", "_blank")); // open galleries in a new tab
+        }
+        if(config.mode === "icon") content.querySelectorAll(".glname").forEach((i) => i.appendChild(document.createElement("span")).innerHTML = "&#x1F441;&#xFE0F;&#x200D;&#x1F5E8;&#xFE0F;");
+        content.querySelectorAll(config.mode === "icon"? ".glname>span:last-child" : ".glink").forEach((g) => g.onpointerover = (e) => {
+            const evtprom = new Promise((resolve) => g.onpointerout = (evt) => resolve(evt)); // promise resolved by pointerout event
+            const tagprom = (async () => {
+                const xhr = await window.fetch(e.target.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href);
+                if(!xhr.ok) throw new TypeError("error " + xhr.status);
+                const response = await xhr.text();
+                tt.replaceChildren(document.createRange().createContextualFragment(response).querySelector("div#taglist>table"));
+                tt.style.setProperty("--t", `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`);
+                tt.style.setProperty("--l", `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`);
+                tt.style.setProperty("--v", "visible");
+            })(); // async functions always return a promise
+            Promise.all([evtprom, tagprom]).then((_) => { // make sure we have both event and tags before dismissing tooltip
+                tt.style.setProperty("--v", "hidden");
+                tt.replaceChildren(); // remove everything
+            }).catch((error) => console.error(error.message));
         });
+        document.querySelector(".itg").appendChild(content); // restore in one shot all page elements after our modifications
     }
-})(Object.freeze({
+})(Object.freeze({ // the default values
     mode: GM_getValue("exhtp.mode", "legacy"),
     views: GM_getValue("exhtp.views", ["m"]),
     newtab: GM_getValue("exhtp.newtab", true)
