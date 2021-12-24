aca84a52e37d85cb84ec20e2a4fa756ddb5a13fb
junker88b <7722100+fp555@users.noreply.github.com>
2021-12-24 15:42:18 +0100

    - SVGs instead of emojis - split resources - update github workflow

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 # E(x)-Hentai Tags Preview :panda_face::label::mag:
-This is an unofficial userscript for e-hentai/exhentai (**NSFW sites** so no links) that displays related tags when you hover over a gallery link. If you can't be bothered opening galleries every time to see all tags and you don't want to use the native *Extended* layout, this is the script for you. This strives to be the smallest and most straightforward script that could possibly do a decent enough job.
+This is an unofficial, simple userscript for e-hentai/exhentai that displays assigned tags when you hover over a gallery link. If you can't be bothered opening galleries every time to check if there's something absolutely disgusting and you don't want to use the native *Extended* layout, this is the script for you.
 
-## Install
-If your browser does not natively support userscripts, install something like [Tampermonkey](https://tampermonkey.net/) or [Violentmonkey](https://violentmonkey.github.io/get-it/), then go grab the [latest userscript release](https://github.com/fp555/exh-tags-preview/releases/latest/download/script.user.js).
+## Installation
+If your browser does not natively support userscripts, install something like [Tampermonkey](https://tampermonkey.net/) or [Violentmonkey](https://violentmonkey.github.io/get-it/), then grab the [latest userscript release](https://github.com/fp555/exh-tags-preview/releases/latest/download/script.user.js).
 
-## Configuration
 The script will autoconfigure the default settings on first run. Starting with v1.8.0 there is an options button at the bottom right corner of the webpage. [Check your release notes](https://github.com/fp555/exh-tags-preview/releases) for more details on available settings.
 
 ## Troubleshooting
-### I found a bug! / It doesn't work!
 Before [opening an issue](https://github.com/fp555/exh-tags-preview/issues/new) please:
-1) Stop using weird and outdated browsers that don't support basic W3C standards or modern CSS/JS features;
+1) Stop using weird and outdated browsers that don't support basic web standards or modern CSS/JS features;
 2) Make sure your userscript manager is updated to the latest available version;
-3) Make sure you are using the latest release of the userscript;
-4) This script uses emojis: [make sure](http://caniemoji.com/) your browser/OS combination can display them properly.
+3) Make sure you are using the [latest release of the userscript](https://github.com/fp555/exh-tags-preview/releases/latest);
+4) Make sure your issue isn't listed in the section below.
 
-### Known issues/limitations
-- The popup will be empty if a gallery has no tags at all. Please be a useful member of the community and add some yourself;
-- Empty popups for galleries that trigger the "offensive content" warning if you have it enabled. Make sure you **clearly** understand the risks before disabling it;
-- Empty popups for inaccessible (permanently deleted, copyright claimed, banned in your country...) galleries.
+### Known issues & limitations
+You will get an empty tooltip (or no tooltip at all) if:
+- A gallery has no tags whatsoever. Please be a useful member of the community and add some yourself;
+- A gallery triggers the "offensive content" warning and you have it enabled. Make sure you *clearly* understand the consequences of disabling it;
+- A gallery is inaccessible (permanently deleted, copyright claimed, banned in your country...).
 
-### Hey why this doesn't have `$feature`?
-[Open an issue](https://github.com/fp555/exh-tags-preview/issues/new) and convince me to work on it. Even better: [create a pull request](https://help.github.com/articles/creating-a-pull-request/).
+### Missing features & requests
+If it's something that's not too unrelated to the purpose of this userscript, [open an issue](https://github.com/fp555/exh-tags-preview/issues/new) and convince me to work on it.
+
+If you end up creating your own version of this userscript consider [creating a pull request](https://help.github.com/articles/creating-a-pull-request/) if you'd like me to merge your changes.
 
 ### Is this spyware? (Privacy policy)
-I couldn't care less about your disgusting fetishes: I already have mine, and I can guarantee they're objectively better than yours :smirk:. Your questionable browsing history shall remain between you and your assigned glow-in-the-dark agent from your domestic brand of alphabet agencies. This userscript was originally made for personal use and convenience, and the idea of collecting user data has never crossed my mind. This userscript has nothing up his sleeve, mainly because it's so small and simple it doesn't even *have* sleeves.
+I couldn't care less about your disgusting fetishes: I already have mine, and I can guarantee they're objectively better than yours :smirk:. Your questionable browsing history shall remain between you and your assigned glow-in-the-dark agent from your domestic mass surveillance agencies. This userscript was originally made for personal use and convenience, and the idea of collecting user data has never crossed my mind.
 
 ### How do I get past the panda?
 LURK MOAR NEWFAG
--- src/script.js
+++ src/script.js
@@ -1,54 +1,49 @@
-((config) => {
+((rtconfig) => {
     "use strict";
     
-    const css = GM_getResourceText("style");
-    const content = document.createRange().createContextualFragment(GM_getResourceText("content"));
-    const curview = document.querySelector("#dms option[selected]").value;
-    const options = {
-        mode: ["legacy","icon"],
-        views: ["m","p","l","t"], // "e"xtended is not supported
-        newtab: [true,false]
-    };
+     // save defaults on 1st run
+    for(const c in rtconfig) GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, rtconfig[c]);
     
-    for(const c in config) { // check for valid storage values
-        GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, config[c]); // set defaults on 1st run
-        (Array.isArray(config[c])? config[c] : [config[c]]).forEach((x) => {
-            if(options[c].includes(x)) content.querySelector(`input[name=${c}][value=${x}]`).checked = true; // init options panel
-            else {
-                console.error("Unrecognized option " + x + " for " + c + ". Clearing from storage");
-                GM_deleteValue("exhtp." + c);
-                return;
-            }
-        });
-    }
-    content.querySelector("form").onchange = (e) => { // option panel wiring
+    // apply css
+    GM_addStyle(GM_getResourceText("style"));
+    
+    // options panel setup
+    const op = document.createRange().createContextualFragment(GM_getResourceText("options"));
+    op.querySelector(`input[name=mode][value=${rtconfig.mode}]`).checked = true;
+    rtconfig.views.split('').forEach(v => op.querySelector(`input[name=views][value=${v}]`).checked = true);
+    op.querySelector("input[name=newtab]").checked = rtconfig.newtab;
+    const ttopt = op.querySelector("#ttopt");
+    ttopt.onchange = e => {
         e.stopPropagation(); // change events will all bubble here, no need to add listeners to all checkboxes
         GM_setValue("exhtp.mode", document.querySelector("input[name=mode]:checked").value);
-        GM_setValue("exhtp.views", [...document.querySelectorAll("input[name=views]:checked")].map((v) => v.value));
+        GM_setValue("exhtp.views", [...document.querySelectorAll("input[name=views]:checked")].map(v => v.value).join(''));
         GM_setValue("exhtp.newtab", document.querySelector("input[name=newtab]").checked);
     };
+    ttopt.classList.add(window.location.href.indexOf("exhentai.org") >= 0? "exstyle" : "ehstyle");
+    document.body.appendChild(op);
+    
+    // open galleries in a new tab
+    if(rtconfig.newtab)
+        document.querySelectorAll(".gl3m.glname>a,.gl3c.glname>a,.gl1e>div>a,.gl2e>div>a,.gl1t>a,.gl4t.glname>div>a,.gl3t>a").forEach(a =>
+            a.setAttribute("target", "_blank"));
     
     /* main */
-    if(config.views.includes(curview)) {
-        GM_addStyle(css);
-        const tt = content.querySelector("#tagstt");
-        const ttopt = content.querySelector("#ttopt");
-        [tt, ttopt].forEach((el) => el.classList.add(window.location.href.indexOf("exhentai.org") >= 0? "exstyle" : "ehstyle"));
-        document.body.appendChild(content); // since it is now empty we can reuse this documentfragment
-        content.replaceChildren(...document.querySelectorAll(".itg>*")); // temp moving page elements outside DOM for performance
-        if(config.newtab) {
-            const sel = ({
-                m: ".gl3m.glname>a",
-                p: ".gl3m.glname>a",
-                l: ".gl3c.glname>a",
-                e: ".gl1e>div>a,.gl2e>div>a",
-                t: ".gl1t>a,.gl4t.glname>div>a,.gl3t>a"
-            })[curview];
-            content.querySelectorAll(sel).forEach((gl) => gl.setAttribute("target", "_blank")); // open galleries in a new tab
+    if(rtconfig.views.includes(document.querySelector("#dms option[selected]").value)) {
+        let gsel = ".glink";
+        
+        // setup tooltip div
+        document.body.appendChild(document.createRange().createContextualFragment(GM_getResourceText("tooltip")));
+        const tt = document.querySelector("#tagstt");
+        tt.classList.add(window.location.href.indexOf("exhentai.org") >= 0? "exstyle" : "ehstyle");
+        
+        // setup icon
+        if(rtconfig.mode === "icon") {
+            const icon = document.createRange().createContextualFragment(GM_getResourceText("icon"));
+            document.querySelectorAll(".glname").forEach(g => g.appendChild(icon.cloneNode(true)));
+            gsel = ".glname>svg";
         }
-        if(config.mode === "icon") content.querySelectorAll(".glname").forEach((i) => i.appendChild(document.createElement("span")).innerHTML = "&#x1F441;&#xFE0F;&#x200D;&#x1F5E8;&#xFE0F;");
-        content.querySelectorAll(config.mode === "icon"? ".glname>span:last-child" : ".glink").forEach((g) => g.onpointerover = (e) => {
-            const evtprom = new Promise((resolve) => g.onpointerout = (evt) => resolve(evt)); // promise resolved by pointerout event
+        document.querySelectorAll(gsel).forEach(g => g.onpointerover = e => {
+            const evtprom = new Promise(resolve => g.onpointerout = evt => resolve(evt)); // promise resolved by pointerout event
             const tagprom = (async () => {
                 const xhr = await window.fetch(e.target.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href);
                 if(!xhr.ok) throw new TypeError("error " + xhr.status);
@@ -58,15 +53,14 @@
                 tt.style.setProperty("--l", `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`);
                 tt.style.setProperty("--v", "visible");
             })(); // async functions always return a promise
-            Promise.all([evtprom, tagprom]).then((_) => { // make sure we have both event and tags before dismissing tooltip
+            Promise.all([evtprom, tagprom]).then(() => { // make sure we have both event and tags before dismissing tooltip
                 tt.style.setProperty("--v", "hidden");
                 tt.replaceChildren(); // remove everything
             }).catch((error) => console.error(error.message));
         });
-        document.querySelector(".itg").appendChild(content); // restore in one shot all page elements after our modifications
     }
-})(Object.freeze({ // the default values
+})({ // the default values
     mode: GM_getValue("exhtp.mode", "legacy"),
-    views: GM_getValue("exhtp.views", ["m"]),
+    views: GM_getValue("exhtp.views", "m"),
     newtab: GM_getValue("exhtp.newtab", true)
-}));
+});
