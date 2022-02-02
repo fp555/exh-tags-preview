85d8be83d141445d0f1af30b0e2b1e4564e6871d
Fabio P <7722100+fp555@users.noreply.github.com>
2022-02-02 23:27:32 +0100

    use json bundle

--- src/script.js
+++ src/script.js
@@ -4,11 +4,12 @@
      // save defaults on 1st run
     for(const c in rtconfig) GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, rtconfig[c]);
     
-    // apply css
-    GM_addStyle(GM_getResourceText("style"));
-    
+	// setup resources
+	GM_addStyle(GM_getResourceText("style"));
+    const content = JSON.parse(GM_getResourceText("content"));
+	
     // options panel setup
-    const op = document.createRange().createContextualFragment(GM_getResourceText("options"));
+    const op = document.createRange().createContextualFragment(content.options);
     op.querySelector(`input[name=mode][value=${rtconfig.mode}]`).checked = true;
     rtconfig.views.split('').forEach(v => op.querySelector(`input[name=views][value=${v}]`).checked = true);
     op.querySelector("input[name=newtab]").checked = rtconfig.newtab;
@@ -26,19 +27,28 @@
     if(rtconfig.newtab)
         document.querySelectorAll(".gl3m.glname>a,.gl3c.glname>a,.gl1e>div>a,.gl2e>div>a,.gl1t>a,.gl4t.glname>div>a,.gl3t>a").forEach(a =>
             a.setAttribute("target", "_blank"));
+			
+	// fix exh my uploads
+	if(window.location.href.indexOf("/upld/manage") >= 0) {
+		document.querySelector("td.gtc5>a:first-child").forEach(a => {
+			const gid = a.href.match(/\d{7}/g)[0];
+			const t = a.href.match(/(\w{10})/g)[0];
+			//TBD
+		};
+	}
     
     /* main */
     if(rtconfig.views.includes(document.querySelector("#dms option[selected]").value)) {
         let gsel = ".glink";
         
         // setup tooltip div
-        document.body.appendChild(document.createRange().createContextualFragment(GM_getResourceText("tooltip")));
+        document.body.appendChild(document.createRange().createContextualFragment(content.tooltip));
         const tt = document.querySelector("#tagstt");
         tt.classList.add(window.location.href.indexOf("exhentai.org") >= 0? "exstyle" : "ehstyle");
         
         // setup icon
         if(rtconfig.mode === "icon") {
-            const icon = document.createRange().createContextualFragment(GM_getResourceText("icon"));
+            const icon = document.createRange().createContextualFragment(content.icon);
             document.querySelectorAll(".glname").forEach(g => g.appendChild(icon.cloneNode(true)));
             gsel = ".glname>svg";
         }
