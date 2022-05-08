221b1ca72ed7863115ab9cffba60d2295242dc07
Fabio P <7722100+fp555@users.noreply.github.com>
2022-05-08 18:54:26 +0200

    Changes for v1.8.2 (#3)
    * add json bundle
    * use json bundle
    * fix stats html
    * add stats links in exh uploads section
    * switch for different webpages
    * fix for exh managegallery stats link
    * more robust my uploads stats links

--- src/script.js
+++ src/script.js
@@ -4,11 +4,12 @@
      // save defaults on 1st run
     for(const c in rtconfig) GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, rtconfig[c]);
     
-    // apply css
+    // setup resources
     GM_addStyle(GM_getResourceText("style"));
-    
+    const content = JSON.parse(GM_getResourceText("content"));
+	
     // options panel setup
-    const op = document.createRange().createContextualFragment(GM_getResourceText("options"));
+    const op = document.createRange().createContextualFragment(content.options);
     op.querySelector(`input[name=mode][value=${rtconfig.mode}]`).checked = true;
     rtconfig.views.split('').forEach(v => op.querySelector(`input[name=views][value=${v}]`).checked = true);
     op.querySelector("input[name=newtab]").checked = rtconfig.newtab;
@@ -19,46 +20,56 @@
         GM_setValue("exhtp.views", [...document.querySelectorAll("input[name=views]:checked")].map(v => v.value).join(''));
         GM_setValue("exhtp.newtab", document.querySelector("input[name=newtab]").checked);
     };
-    ttopt.classList.add(window.location.href.indexOf("exhentai.org") >= 0? "exstyle" : "ehstyle");
+    ttopt.classList.add(window.location.hostname === "exhentai.org"? "exstyle" : "ehstyle");
     document.body.appendChild(op);
-    
-    // open galleries in a new tab
-    if(rtconfig.newtab)
-        document.querySelectorAll(".gl3m.glname>a,.gl3c.glname>a,.gl1e>div>a,.gl2e>div>a,.gl1t>a,.gl4t.glname>div>a,.gl3t>a").forEach(a =>
-            a.setAttribute("target", "_blank"));
-    
-    /* main */
-    if(rtconfig.views.includes(document.querySelector("#dms option[selected]").value)) {
-        let gsel = ".glink";
+
+    switch(window.location.pathname) {
+	case "/upld/manage": // fix exh my uploads
+		document.querySelectorAll("td.gtc5>a:first-child").forEach(a1 => {
+			const stats = document.createRange().createContextualFragment(content.stats);
+			const glink = a1.pathname.split('/');
+			stats.querySelector("a").search = `?gid=${glink[2]}&t=${glink[3]}`;
+			a1.after(stats);
+		});
+		break;
         
-        // setup tooltip div
-        document.body.appendChild(document.createRange().createContextualFragment(GM_getResourceText("tooltip")));
-        const tt = document.querySelector("#tagstt");
-        tt.classList.add(window.location.href.indexOf("exhentai.org") >= 0? "exstyle" : "ehstyle");
+	case "/upld/managegallery": // fix exh stats link
+		document.querySelector("a[href*='stats.php']").hostname = "e-hentai.org";
+		break;
         
-        // setup icon
-        if(rtconfig.mode === "icon") {
-            const icon = document.createRange().createContextualFragment(GM_getResourceText("icon"));
-            document.querySelectorAll(".glname").forEach(g => g.appendChild(icon.cloneNode(true)));
-            gsel = ".glname>svg";
-        }
-        document.querySelectorAll(gsel).forEach(g => g.onpointerover = e => {
-            const evtprom = new Promise(resolve => g.onpointerout = evt => resolve(evt)); // promise resolved by pointerout event
-            const tagprom = (async () => {
-                const xhr = await window.fetch(e.target.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href);
-                if(!xhr.ok) throw new TypeError("error " + xhr.status);
-                const response = await xhr.text();
-                tt.replaceChildren(document.createRange().createContextualFragment(response).querySelector("div#taglist>table"));
-                tt.style.setProperty("--t", `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`);
-                tt.style.setProperty("--l", `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`);
-                tt.style.setProperty("--v", "visible");
-            })(); // async functions always return a promise
-            Promise.all([evtprom, tagprom]).then(() => { // make sure we have both event and tags before dismissing tooltip
-                tt.style.setProperty("--v", "hidden");
-                tt.replaceChildren(); // remove everything
-            }).catch((error) => console.error(error.message));
-        });
-    }
+	default: // setup tooltip div
+		if(rtconfig.views.includes(document.querySelector("#dms option[selected]").value)) {
+			let gsel = ".glink";
+			document.body.appendChild(document.createRange().createContextualFragment(content.tooltip));
+			const tt = document.querySelector("#tagstt");
+			tt.classList.add(window.location.hostname === "exhentai.org"? "exstyle" : "ehstyle");
+
+			if(rtconfig.mode === "icon") { // setup icon
+				document.querySelectorAll(".glname").forEach(g => g.appendChild(document.createRange().createContextualFragment(content.icon)));
+				gsel = ".glname>svg";
+			}
+			document.querySelectorAll(gsel).forEach(g => g.onpointerover = e => {
+				const eoutprom = new Promise(resolve => g.onpointerout = eout => resolve(eout));
+				const tagprom = async () => {
+					const xhr = await window.fetch(e.target.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href);
+					if(!xhr.ok) throw new TypeError("error " + xhr.status);
+					const response = await xhr.text();
+					tt.replaceChildren(document.createRange().createContextualFragment(response).querySelector("div#taglist>table"));
+					tt.style.setProperty("--t", `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`);
+					tt.style.setProperty("--l", `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`);
+					tt.style.setProperty("--v", "visible");
+				}; // async functions always return a promise
+				Promise.all([eoutprom, tagprom()]).then(() => { // make sure we have both event and tags before dismissing tooltip
+					tt.style.setProperty("--v", "hidden");
+					tt.replaceChildren(); // remove everything
+				}).catch((error) => console.error(error.message));
+			});
+		};
+	
+		// open galleries in a new tab
+		if(rtconfig.newtab) document.querySelectorAll(".gl3m.glname>a,.gl3c.glname>a,.gl1e>div>a,.gl2e>div>a,.gl1t>a,.gl4t.glname>div>a,.gl3t>a").forEach(a => a.setAttribute("target", "_blank"));
+		break;
+    };
 })({ // the default values
     mode: GM_getValue("exhtp.mode", "legacy"),
     views: GM_getValue("exhtp.views", "m"),
