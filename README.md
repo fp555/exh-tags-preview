79865f7071eee1805f0582fecae56e22cb7d93a3
Fabio P. <7722100+fp555@users.noreply.github.com>
2024-07-13 14:27:47 +0200

    Merge branch dev into master

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # E(x)-Hentai Tags Preview :panda_face::label::mag:
-This is an unofficial, simple userscript for e-hentai/exhentai that displays assigned tags when you hover over a gallery link. If you can't be bothered opening galleries every time to check if there's something absolutely disgusting and you don't want to use the native *Extended* layout, this is the script for you.
+This is an **unofficial** userscript, compatible with both panda websites, that shows a table with all tags assigned to a gallery that seamlessly blends with native page layouts. If you don't like the native *Extended* view, and others don't give you enough information, give this a try.
 
 ## Installation
 If your browser does not natively support userscripts, install something like [Tampermonkey](https://tampermonkey.net/) or [Violentmonkey](https://violentmonkey.github.io/get-it/), then grab the [latest userscript release](https://github.com/fp555/exh-tags-preview/releases/latest/download/script.user.js).
 
-The script will autoconfigure the default settings on first run. Starting with v1.8.0 there is an options button at the bottom right corner of the webpage. [Check your release notes](https://github.com/fp555/exh-tags-preview/releases) for more details on available settings.
+This userscript has some configuration options, for your convenience you can change them from the :gear: icon that gets added next to the layout selector. [Check your release notes](https://github.com/fp555/exh-tags-preview/releases) for more details on available settings.
 
 ## Troubleshooting
 Before [opening an issue](https://github.com/fp555/exh-tags-preview/issues/new) please:
@@ -14,18 +14,18 @@ Before [opening an issue](https://github.com/fp555/exh-tags-preview/issues/new) 
 4) Make sure your issue isn't listed in the section below.
 
 ### Known issues & limitations
-You will get an empty tooltip (or no tooltip at all) if:
+You could get an empty tooltip (or no tooltip at all) if:
 - A gallery has no tags whatsoever. Please be a useful member of the community and add some yourself;
-- A gallery triggers the "offensive content" warning and you have it enabled. Make sure you *clearly* understand the consequences of disabling it;
-- A gallery is inaccessible (permanently deleted, copyright claimed, banned in your country...).
+- A gallery is inaccessible (permanently deleted, copyright claimed, banned in your country...);
+- E-H API is acting up (check your browser console).
 
 ### Missing features & requests
-If it's something that's not too unrelated to the purpose of this userscript, [open an issue](https://github.com/fp555/exh-tags-preview/issues/new) and convince me to work on it.
+If it's something that's not too unrelated to the purpose of this userscript, [open an issue](https://github.com/fp555/exh-tags-preview/issues/new) and convince me to work on it. If you look closely you can see I already added tiny little fixes that are unrelated to displaying tags.
 
 If you end up creating your own version of this userscript consider [creating a pull request](https://help.github.com/articles/creating-a-pull-request/) if you'd like me to merge your changes.
 
-### Is this spyware? (Privacy policy)
-I couldn't care less about your disgusting fetishes: I already have mine, and I can guarantee they're objectively better than yours :smirk:. Your questionable browsing history shall remain between you and your assigned glow-in-the-dark agent from your domestic mass surveillance agencies. This userscript was originally made for personal use and convenience, and the idea of collecting user data has never crossed my mind.
+### Is this spyware?
+I couldn't care less about your disgusting fetishes: I already have mine, and I can guarantee they're objectively better than yours :smirk:. Your questionable browsing history shall remain between you and your assigned glow-in-the-dark agent, 'cause I don't want anything to do with it. This userscript was originally made for personal use and convenience, and the idea of collecting user data has never crossed my mind. Luckily for you the source code is right here, you can just, like... read it or something.
 
 ### How do I get past the panda?
 LURK MOAR NEWFAG
--- src/script.js
+++ src/script.js
@@ -11,29 +11,14 @@
     const content = JSON.parse(GM_getResourceText("content"));
     GM_addStyle(content.style);
 
-    // options panel setup
-    const op = document.createRange().createContextualFragment(content.options);
-    op.querySelector(`input[name=mode][value=${args.mode}]`).checked = true;
-    args.views.split('').forEach(v => op.querySelector(`input[name=views][value=${v}]`).checked = true);
-    op.querySelector("input[name=newtab]").checked = args.newtab;
-    const ttopt = op.querySelector("#ttopt");
-    ttopt.onchange = function(e) {
-        e.stopPropagation(); // change events will all bubble here, no need to add listeners to all checkboxes
-        GM_setValue("exhtp.mode", document.querySelector("input[name=mode]:checked").value);
-        GM_setValue("exhtp.views", [...document.querySelectorAll("input[name=views]:checked")].map(v => v.value).join(''));
-        GM_setValue("exhtp.newtab", document.querySelector("input[name=newtab]").checked);
-    };
-    ttopt.classList.add(exh? "exstyle" : "ehstyle");
-    document.body.appendChild(op);
-	
 	// main
     switch(window.location.pathname) {
 	case "/upld/manage": // fix exh my uploads
-		document.querySelectorAll("td.gtc5>a:first-child").forEach(a1 => {
+		document.querySelectorAll("td.gtc5>a:first-child").forEach(a => {
 			const stats = document.createRange().createContextualFragment(content.stats);
-			const glink = a1.pathname.split('/');
+			const glink = a.pathname.split('/');
 			stats.querySelector("a").search = `?gid=${glink[2]}&t=${glink[3]}`;
-			a1.after(stats);
+			a.after(stats);
 		});
 		break;
 
@@ -41,64 +26,86 @@
 		document.querySelector("a[href*='stats.php']").hostname = "e-hentai.org";
 		break;
 
-	default: // setup tooltip
-		if(args.views.includes(document.querySelector(".searchnav > div:last-child > select").value)) {
-			let gsel = ".glink";
-			document.body.appendChild(document.createRange().createContextualFragment(content.tooltip));
-			const tt = document.querySelector("#tagstt");
-			tt.classList.add(exh? "exstyle" : "ehstyle");
-			// setup icon
-			if(args.mode === "icon") {
-				document.querySelectorAll(".gl3m.glname,.gl3c.glname,.gl6t").forEach(g => g.appendChild(document.createRange().createContextualFragment(content.icon)));
-				gsel = ".svgicon";
-			}
-			document.querySelectorAll(gsel).forEach(g => g.onpointerover = e => {
-				const eoutprom = new Promise(resolve => g.onpointerout = eout => resolve(eout));
-				const tagprom = async () => {
-					// fetch gallery tags
-					const xhr = await fetch("https://api.e-hentai.org/api.php", {
-						method: "POST",
-						headers: {"Content-Type": "application/json;charset=utf-8"},
-						body: JSON.stringify({
-							method: "gdata",
-							namespace: 1,
-							gidlist: [e.target.closest("td.gl3m,td.gl3c,div.gl1t").querySelector("a").href.match(/(\d+)\/(\w+)\/$/).splice(1)]
-						})
-					});
-					if(!xhr.ok) throw new TypeError("error " + xhr.status);
-					xhr.json().then(response => {
-						const tagsmap = new Map();
-						response.gmetadata[0].tags.map(t => t.match(/(.+):(.+)/).slice(1)).forEach(([k, v]) => tagsmap.set(k, tagsmap.has(k)? [...tagsmap.get(k), v] : [v]));
-						// create tags table
-						[...tagsmap.keys()].forEach(k => {
-							const tds = [document.createElement("td"), document.createElement("td")];
-							tds[0].classList.add("tc");
-							tds[0].append(`${k}:`);
-							tagsmap.get(k).forEach(v => {
-								const vdiv = document.createElement("div");
-								vdiv.classList.add("gt");
-								vdiv.append(`${v}`);
-								tds[1].append(vdiv);
-							});
-							tt.querySelector("tbody").appendChild(document.createElement("tr")).append(...tds);
-						});
-						tt.style.setProperty("--t", `${(window.innerHeight - e.clientY < tt.offsetHeight)? e.pageY - tt.offsetHeight : e.pageY}px`);
-						tt.style.setProperty("--l", `${(window.innerWidth - e.clientX < tt.offsetWidth)? e.pageX - tt.offsetWidth : e.pageX}px`);
-						tt.style.setProperty("--v", "visible");
-					});
-				}; // async functions always return a promise
-				Promise.all([eoutprom, tagprom()]).then(() => { // make sure we have both event and tags before dismissing tooltip
-					tt.style.setProperty("--v", "hidden");
-					tt.querySelector("tbody").replaceChildren(); // remove everything
-				}).catch((error) => console.error(error.message));
-			});
+	default:
+		// options panel setup
+		const op = document.createRange().createContextualFragment(content.options);
+		args.views.split('').forEach(v => op.querySelector(`input[name=views][value=${v}]`).checked = true);
+		op.querySelector("input[name=newtab]").checked = args.newtab;
+		const ttopt = op.querySelector("#ttopt");
+		ttopt.onchange = function(e) {
+			e.stopPropagation(); // change events will all bubble here
+			GM_setValue("exhtp.views", [...document.querySelectorAll("input[name=views]:checked")].map(v => v.value).join(''));
+			GM_setValue("exhtp.newtab", document.querySelector("input[name=newtab]").checked);
 		};
+		ttopt.classList.add(exh? "exstyle" : "ehstyle");
+		document.querySelector(".searchnav>div:last-child").prepend(op);
+		
+		// setup icon & tooltip
+		if(args.views.includes(document.querySelector(".searchnav select").value)) {
+			document.querySelectorAll(".gl3m.glname,.gl3c.glname,.gl6t").forEach(g => g.append(document.createRange().createContextualFragment(content.tooltip)));
+			document.querySelectorAll(".tticon>.tagstt").forEach(t => t.classList.add(exh? "exstyle" : "ehstyle"));
+			document.querySelectorAll(".tticon").forEach(i => i.onclick = (ev) => {
+				ev.stopPropagation();
+				if(ev.detail === 1) { // only fire on single click events
+					const getTags = new Promise(resolve => {
+						if(i.hasAttribute("data-tags")) resolve(false); // if we already inserted tags, stop
+						
+						// if not, fetch tags from E-H API (https://ehwiki.org/wiki/API)
+						else fetch("https://api.e-hentai.org/api.php", {
+							method: "POST",
+							headers: {"Content-Type": "application/json;charset=utf-8"},
+							body: JSON.stringify({
+								method: "gdata",
+								namespace: 1,
+								gidlist: [i.parentElement.querySelector("a").href.match(/(\d+)\/(\w+)\/$/).splice(1)]
+							})
+						}).then(result => {
+							if(!result.ok) throw new Error("fetch: error " + xhr.status);
+							return result.json();
+						}).then(response => {
+							
+							// our API returns a simple array of strings "namespace:tag"
+							// to make it easier to work with, we transform it into a Map
+							// where namespaces are keys, and values are arrays of tags
+							const tagsmap = new Map();
+							response.gmetadata[0].tags.map(t => t.match(/(.+):(.+)/).slice(1)).forEach(([k, v]) => tagsmap.set(k, tagsmap.has(k)? [...tagsmap.get(k), v] : [v]));
+
+							// create tags table
+							const tt = document.createDocumentFragment();
+							[...tagsmap.keys()].forEach(k => {
+								const tds = [document.createElement("td"), document.createElement("td")];
+								tds[0].classList.add("tc");
+								tds[0].append(`${k}:`);
+								tagsmap.get(k).forEach(v => {
+									const vs = document.createElement("span");
+									vs.classList.add("stag");
+									vs.append(`${v}`);
+									tds[1].append(vs);
+								});
+								tt.appendChild(document.createElement("tr")).append(...tds);
+							});
+							resolve(tt);
+						});
+					});
+					getTags.then(ttags => {
+						if(ttags) i.querySelector("tbody").replaceChildren(ttags);
+
+						// adjust tooltip positioning
+						const tt = i.querySelector(".tagstt");
+						tt.style.setProperty("bottom", (window.innerHeight - ev.clientY < tt.offsetHeight)? "50%" : "auto"); //up
+						tt.style.setProperty("top", (window.innerHeight - ev.clientY < tt.offsetHeight)? "auto" : "50%"); //down
+						tt.style.setProperty("right", (window.innerWidth - ev.clientX < tt.offsetWidth)? "50%" : "auto"); //left
+						tt.style.setProperty("left", (window.innerWidth - ev.clientX < tt.offsetWidth)? "auto" : "50%"); //right
+						i.toggleAttribute("data-tags", true);
+					});
+				}
+			});
+		}
 		// open galleries in a new tab
 		if(args.newtab) document.querySelectorAll(".gl3m.glname>a,.gl3c.glname>a,.gl1e>div>a,.gl2e>div>a,.gl1t>a,.gl4t.glname>div>a,.gl3t>a").forEach(a => a.setAttribute("target", "_blank"));
 		break;
     };
 })({ // the default values
-    mode: GM_getValue("exhtp.mode", "legacy"),
     views: GM_getValue("exhtp.views", "m"),
     newtab: GM_getValue("exhtp.newtab", true)
 });
