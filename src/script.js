e2cc21ebc1ca8964e596ac5c34fa22f47aba82a1
Fabio P. <7722100+fp555@users.noreply.github.com>
2024-11-24 17:09:39 +0100

    Add title attribute to tags

--- src/script.js
+++ src/script.js
@@ -4,14 +4,13 @@
      // save defaults on 1st run
     for(const c in args) GM_getValue("exhtp." + c, false) || GM_setValue("exhtp." + c, args[c]);
 
-    // are we on fjords?
+    // is it ExH or E-H?
     const exh = location.host.substr(0,2) === "ex";
 
     // setup resources and CSS
     const content = JSON.parse(GM_getResourceText("content"));
     GM_addStyle(content.style);
 
-	// main
     switch(window.location.pathname) {
 	case "/upld/manage": // fix exh my uploads
 		document.querySelectorAll("td.gtc5>a:first-child").forEach(a => {
@@ -39,72 +38,68 @@
 		};
 		ttopt.classList.add(exh? "exstyle" : "ehstyle");
 		document.querySelector(".searchnav>div:last-child").prepend(op);
-		
 		// setup icon & tooltip
 		if(args.views.includes(document.querySelector(".searchnav select").value)) {
 			document.querySelectorAll(".gl3m.glname,.gl3c.glname,.gl6t").forEach(g => g.append(document.createRange().createContextualFragment(content.tooltip)));
 			document.querySelectorAll(".tticon>.tagstt").forEach(t => t.classList.add(exh? "exstyle" : "ehstyle"));
 			document.querySelectorAll(".tticon").forEach(i => i.onclick = (ev) => {
 				ev.stopPropagation();
-				if(ev.detail === 1) { // only fire on single click events
-					const getTags = new Promise(resolve => {
-						if(i.hasAttribute("data-tags")) resolve(false); // if we already inserted tags, stop
-						
-						// if not, fetch tags from E-H API (https://ehwiki.org/wiki/API)
-						else fetch("https://api.e-hentai.org/api.php", {
-							method: "POST",
-							headers: {"Content-Type": "application/json;charset=utf-8"},
-							body: JSON.stringify({
-								method: "gdata",
-								namespace: 1,
-								gidlist: [i.closest(".gl3m,.gl3c,.gl1t").querySelector("a").href.match(/(\d+)\/(\w+)\/$/).splice(1)]
-							})
-						}).then(result => {
-							if(!result.ok) throw new Error("fetch: error " + xhr.status);
-							return result.json();
-						}).then(response => {
-							
-							// our API returns a simple array of strings "namespace:tag"
-							// to make it easier to work with, we transform it into a Map
-							// where namespaces are keys, and values are arrays of tags
-							const tagsmap = new Map();
-							response.gmetadata[0].tags.map(t => t.match(/(.+):(.+)/).slice(1)).forEach(([k, v]) => tagsmap.set(k, tagsmap.has(k)? [...tagsmap.get(k), v] : [v]));
-
-							// create tags table
-							const tt = document.createDocumentFragment();
-							[...tagsmap.keys()].forEach(k => {
-								const tds = [document.createElement("td"), document.createElement("td")];
-								tds[0].classList.add("tc");
-								tds[0].append(`${k}:`);
-								tagsmap.get(k).forEach(v => {
-									const vs = document.createElement("span");
-									vs.classList.add("stag");
-									vs.append(`${v}`);
-									tds[1].append(vs);
-								});
-								tt.appendChild(document.createElement("tr")).append(...tds);
-							});
-							resolve(tt);
-						});
-					});
-					getTags.then(ttags => {
-						if(ttags) i.querySelector("tbody").replaceChildren(ttags);
-
-						// adjust tooltip positioning
-						const tt = i.querySelector(".tagstt");
-						tt.style.setProperty("bottom", (window.innerHeight - ev.clientY < tt.offsetHeight)? "50%" : "auto"); //up
-						tt.style.setProperty("top", (window.innerHeight - ev.clientY < tt.offsetHeight)? "auto" : "50%"); //down
-						tt.style.setProperty("right", (window.innerWidth - ev.clientX < tt.offsetWidth)? "50%" : "auto"); //left
-						tt.style.setProperty("left", (window.innerWidth - ev.clientX < tt.offsetWidth)? "auto" : "50%"); //right
-						i.toggleAttribute("data-tags", true);
-					});
-				}
+				// only fetch if single click event and tags not already inserted
+				if(ev.detail === 1 && !i.hasAttribute("data-tags")) getTags(i.closest(".gl3m,.gl3c,.gl1t").querySelector("a").href.match(/(\d+)\/(\w+)\/$/).splice(1)).then(response => {
+					i.querySelector("tbody").replaceChildren(tagsTable(response));
+					// adjust tooltip positioning
+					const tt = i.querySelector(".tagstt");
+					tt.style.setProperty("bottom", (window.innerHeight - ev.clientY < tt.offsetHeight)? "50%" : "auto"); //up
+					tt.style.setProperty("top", (window.innerHeight - ev.clientY < tt.offsetHeight)? "auto" : "50%"); //down
+					tt.style.setProperty("right", (window.innerWidth - ev.clientX < tt.offsetWidth)? "50%" : "auto"); //left
+					tt.style.setProperty("left", (window.innerWidth - ev.clientX < tt.offsetWidth)? "auto" : "50%"); //right
+					i.toggleAttribute("data-tags", true);
+				});
 			});
 		}
 		// open galleries in a new tab
 		if(args.newtab) document.querySelectorAll(".gl3m.glname>a,.gl3c.glname>a,.gl1e>div>a,.gl2e>div>a,.gl1t>a,.gl4t.glname>div>a,.gl3t>a").forEach(a => a.setAttribute("target", "_blank"));
 		break;
-    };
+    }
+	
+	// fetch tags from E-H API (https://ehwiki.org/wiki/API)
+	const getTags = async (gid) => {
+		const res = await fetch("https://api.e-hentai.org/api.php", {
+			method: "POST",
+			headers: {"Content-Type": "application/json;charset=utf-8"},
+			body: JSON.stringify({
+				method: "gdata",
+				namespace: 1,
+				gidlist: [gid]
+			})
+		});
+		if(!res.ok) throw new Error("fetch: error " + xhr.status);
+		else return await res.json();
+	}
+	
+	// assemble tooltip contents
+	const tagsTable = (resp) => {
+		// E-H API returns a simple array of strings "namespace:tag"
+		// For convenience we transform it into a Map where
+		// namespaces are keys, and values are arrays of tags
+		const tagsmap = new Map();
+		resp.gmetadata[0].tags.map(t => t.match(/(.+):(.+)/).slice(1)).forEach(([k, v]) => tagsmap.set(k, tagsmap.has(k)? [...tagsmap.get(k), v] : [v]));
+		const tt = document.createDocumentFragment(); //tags table
+		[...tagsmap.keys()].forEach(k => {
+			const tds = [document.createElement("td"), document.createElement("td")];
+			tds[0].classList.add("tc");
+			tds[0].append(`${k}:`);
+			tagsmap.get(k).forEach(v => {
+				const vs = document.createElement("span");
+				vs.classList.add("stag");
+				vs.title = `${k}:${v}`
+				vs.append(`${v}`);
+				tds[1].append(vs);
+			});
+			tt.appendChild(document.createElement("tr")).append(...tds);
+		});
+		return tt;
+	}
 })({ // the default values
     views: GM_getValue("exhtp.views", "m"),
     newtab: GM_getValue("exhtp.newtab", true)
